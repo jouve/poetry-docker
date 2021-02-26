444684fdc86e925b985855e2f4faeb42bc7abd0d
Cyril Jouve <jv.cyril@gmail.com>
2021-02-26 22:55:57 +0100

    init

---
+++ update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+if ! test -w /var/run/docker.sock; then
+  SUDO=sudo
+else
+  SUDO=
+fi
+
+if docker container inspect cache_cache_1 &>/dev/null; then
+  cache=--volumes-from=cache_cache_1
+else
+  cache=
+fi
+
+$SUDO docker run \
+  $cache \
+  --volume $PWD:/srv \
+  --workdir /srv \
+  alpine:3.13.0 sh -x -c "
+set -e
+apk add --no-cache alpine-conf
+setup-apkcache /var/cache/apk
+apk add --no-cache cargo gcc libffi-dev musl-dev openssl-dev python3-dev;
+python3 -m venv /usr/share/poetry
+/usr/share/poetry/bin/pip install -r poetry.txt
+/usr/share/poetry/bin/poetry lock
+/usr/share/poetry/bin/poetry export --without-hashes > poetry.txt
+"
