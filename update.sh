6dc938e7fb25da6ba3a95c35764b50b5eae6aca2
Cyril Jouve <jv.cyril@gmail.com>
2022-12-11 12:42:00 +0100

    new lock fmt

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 sudo nerdctl run \
   --volume $PWD:/srv \
   --workdir /srv \
-  jouve/poetry:1.2.2-alpine3.17.0 sh -x -c '
+  jouve/poetry:1.3.0-alpine3.17.0 sh -x -c '
 poetry run pip install --upgrade pip setuptools wheel
 poetry run pip freeze --all > pip.txt
 poetry lock
