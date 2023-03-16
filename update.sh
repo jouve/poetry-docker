82234add9d63c8ca3d143ba6a2e516580843511d
Cyril Jouve <jv.cyril@gmail.com>
2023-03-16 23:05:05 +0100

    poetry 1.4.0

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 sudo nerdctl run \
   --volume $PWD:/srv \
   --workdir /srv \
-  jouve/poetry:1.3.2-alpine3.17.2 sh -x -c '
+  jouve/poetry:1.4.0-alpine3.17.2 sh -x -c '
 poetry run pip install --upgrade pip setuptools wheel
 poetry run pip freeze --all > pip.txt
 poetry lock
